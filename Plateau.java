ecf92735b0d790ce33df05ea74a89550a4e87483
colineto <colineto42@gmail.com>
2015-03-22 08:57:15 +0100

    Create Plateau.java

---
+++ Plateau.java
@@ -0,0 +1,23 @@
+import java.awt.Point;
+
+public class Plateau {
+	
+	private Point size;
+	private Piece pieces[][];
+	
+	public Plateau(int x,int y) {
+		
+		size = new Point(x,y);
+		
+		for(int i=0; i<x ; i++){
+			for(int j=0; j<y ; j++){
+				//pieces[j][i] = new Piece(new Face(),new Face(),new Face(),new Face());
+	        }
+        }
+		
+		
+		
+		
+	}
+
+}
